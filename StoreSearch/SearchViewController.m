d9168b2092e1fa486b1b678a4bae7726e135c0c3
ted zhang <yongzhang81@gmail.com>
2014-02-26 22:10:00 +0800

    add search bar & tableview in the search view controller

--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -8,11 +8,15 @@
 
 #import "SearchViewController.h"
 
-@interface SearchViewController ()
+@interface SearchViewController () <UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate>
+
 
 @end
 
 @implementation SearchViewController
+{
+    NSMutableArray *_searchResults;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,12 +31,63 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (_searchResults == nil)
+        return 0;
+    return [_searchResults count];
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifer = @"SearchResultCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifer];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifer];
+    }
+    if ([_searchResults count] == 0)
+    {
+        cell.textLabel.text = @"Nothing found";
+        cell.detailTextLabel.text = @"";
+    }
+    else
+    {
+        cell.textLabel.text = _searchResults[indexPath.row];
+    }
+    return cell;
+}
+
+-(NSIndexPath*)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ([_searchResults count] == 0)
+    {
+        return nil;
+    }
+    return indexPath;
+}
+
+
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar resignFirstResponder];
+    NSLog(@"the search text is %@",searchBar.text);
+}
+
+-(UIBarPosition) positionForBar:(id<UIBarPositioning>)bar
+{
+    return UIBarPositionTop;
 }
 
 @end
